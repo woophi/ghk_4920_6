01cbd79b0c76b6ccec04b67abee047e6fc7a04cf
Konstantin Mikheev <mik.kon6610@gmail.com>
2025-04-11 07:03:11 +0200

    fix

--- src/thx/ThxLayout.tsx
+++ src/thx/ThxLayout.tsx
@@ -1,8 +1,14 @@
 import { Typography } from '@alfalab/core-components/typography';
 import rocket from '../assets/rocket.png';
+import { useTimeout } from '../hooks/useTimeout';
 import { thxSt } from './style.css';
 
 export const ThxLayout = () => {
+  useTimeout(() => {
+    window.location.replace(
+      'alfabank:///sdui_screen?endpoint=v1%2Fgrowthhack-widget-experiment%2Fwidgets%2F0c1eaaaa-f56e-4e81-8f3a-1043f0025e0f&presentationTypeWeb=PRESENT&screenName=gh-exp-final',
+    );
+  }, 2000);
   return (
     <>
       <div className={thxSt.container}>
