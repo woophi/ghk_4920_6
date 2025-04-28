d221f16af4fc9964e06863a9cc6e75395df8b1a5
Konstantin Mikheev <mik.kon6610@gmail.com>
2025-04-28 09:05:20 +0200

    fix

--- src/thx/ThxLayout.tsx
+++ src/thx/ThxLayout.tsx
@@ -6,7 +6,7 @@ import { thxSt } from './style.css';
 export const ThxLayout = () => {
   useTimeout(() => {
     window.location.replace(
-      'alfabank:///sdui_screen?endpoint=v1%2Fgrowthhack-widget-experiment%2Fwidgets%2F0c1eaaaa-f56e-4e81-8f3a-1043f0025e0f&presentationTypeWeb=PRESENT&screenName=gh-exp-final',
+      'alfabank://sdui_screen?endpoint=v1%2Fgrowthhack-widget-experiment%2Fwidgets%2F0c1eaaaa-f56e-4e81-8f3a-1043f0025e0f&presentationTypeWeb=PRESENT&title=%D0%98%D1%81%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5&screenName=gh_4_final',
     );
   }, 2000);
   return (
